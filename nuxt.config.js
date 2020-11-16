437a5fa7ed3d0224f56df075edc9b11d4a2f8322
Frank Halbach <frank.halbach@visteon.com>
2020-11-16 14:29:41 +0100

    update packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,8 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
-  mode: 'spa',
+  //mode: 'spa', //deprecated
+  ssr :false,
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
